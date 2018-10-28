e68a6a532f29be3fb8e5e9585a27e6cf33c26da5
Chris Jensen <zigs@zigs.dk>
2018-10-28 18:21:16 +0100

    Problem 9 solution

--- source/DailyCodingProblem/Problems/Problem9/Solution/NearFarSolution.cs
+++ source/DailyCodingProblem/Problems/Problem9/Solution/NearFarSolution.cs
@@ -4,6 +4,24 @@ namespace DailyCodingProblem.Problems.Problem9.Solution
 {
     public class NearFarSolution : ISolution
     {
-        public int Execute(int[] input) => throw new NotImplementedException();        
+        public int Execute(int[] input)
+        {
+            if (input == null)
+                return 0;
+
+            int far = 0, between = 0, near = 0, unavailable = 0;
+
+            for (int index = input.Length - 1; index >= 0; index--)
+            {
+                int newValue = input[index] + Math.Max(near, Math.Max(between, far));
+
+                far = between;
+                between = near;
+                near = unavailable;
+                unavailable = newValue;
+            }
+
+            return Math.Max(near, unavailable);
+        }
     }
 }
