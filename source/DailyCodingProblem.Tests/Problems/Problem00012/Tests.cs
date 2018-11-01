e911ef7feeaeb2a948232f35289a03183bd8bea1
Chris Jensen <zigs@zigs.dk>
2018-11-01 14:47:51 +0100

    problem 12 tests

---
+++ source/DailyCodingProblem.Tests/Problems/Problem00012/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+using DailyCodingProblem.Problems.Problem00012;
+
+namespace DailyCodingProblem.Tests.Problems.Problem00012
+{
+    public class Tests
+    {
+        public void GivenTest(ISolution solution)
+            => Assert.Equal(5, solution.Solve(4, new int[] { 1, 2 }));
+
+        public void DifferentStepSize(ISolution solution)
+            => Assert.Equal(7, solution.Solve(10, new int[] { 3, 2 }));
+    }
+}
