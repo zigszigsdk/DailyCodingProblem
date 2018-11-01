24619fb4570f2fc7ca17079d66ac865f53cd8054
Chris Jensen <zigs@zigs.dk>
2018-11-01 14:48:12 +0100

    problem 12 solution: Array cache

---
+++ source/DailyCodingProblem.Tests/Problems/Problem00012/Solutions/ArrayCacheTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+using DailyCodingProblem.Problems.Problem00012;
+using DailyCodingProblem.Problems.Problem00012.Solutions;
+
+namespace DailyCodingProblem.Tests.Problems.Problem00012.Solutions
+{
+    public class ArrayCacheTest
+    {
+        ISolution solution = new ArrayCache();
+        Tests tests = new Tests();
+
+        [Fact]
+        public void GivenTest() => tests.GivenTest(solution);
+
+        [Fact]
+        public void DifferentStepSize() => tests.DifferentStepSize(solution);
+    }
+}
---
+++ source/DailyCodingProblem/Problems/Problem00012/Solutions/ArrayCache.cs
@@ -0,0 +1,24 @@
+﻿
+namespace DailyCodingProblem.Problems.Problem00012.Solutions
+{
+    public class ArrayCache : ISolution
+    {
+        public int Solve(int totalSteps, int[] steppingSizes)
+        {
+            int[] cache = new int[totalSteps+1]; //including the "0th" step.
+
+            cache[totalSteps] = 1;
+
+            for (int standingAt = totalSteps - 1; standingAt >= 0; standingAt--)
+            {
+                cache[standingAt] = 0;
+
+                foreach (int stepSize in steppingSizes)
+                    if (standingAt + stepSize < cache.Length)
+                        cache[standingAt] += cache[standingAt + stepSize];
+            }
+
+            return cache[0];
+        }
+    }
+}
