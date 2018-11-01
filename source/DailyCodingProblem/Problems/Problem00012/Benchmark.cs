646b1d9a0f9f0ef139379920e6de59ae6bbc7032
Chris Jensen <zigs@zigs.dk>
2018-11-01 14:48:27 +0100

    problem 12 benchmark

---
+++ source/DailyCodingProblem/Problems/Problem00012/Benchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using DailyCodingProblem.Problems.Problem00012.Solutions;
+
+namespace DailyCodingProblem.Problems.Problem00012
+{
+    [MemoryDiagnoser]
+    public class Benchmark : IBenchmark
+    {
+        public string printBefore() => "";
+        public string printAfter() => "";
+
+        [Params(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)]
+        public int steps;
+
+        [Params(new int[] {1}, new int[] { 1, 2 }, new int[] {1, 2, 3})]
+        public int[] stepSizes;
+
+        ISolution arrayCache = new ArrayCache();
+
+        [Benchmark]
+        public int ArrayCache() => arrayCache.Solve(steps, stepSizes);
+    }
+}
