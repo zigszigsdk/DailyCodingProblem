85c4cfe3f37ae413b5b5634473d4bcbf3248c1d2
Chris Jensen <zigs@zigs.dk>
2018-11-01 14:09:47 +0100

    problem 11 benchmark

---
+++ source/DailyCodingProblem/Problems/Problem00011/Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using DailyCodingProblem.Problems.Problem00011.Solutions.SearchTree;
+
+namespace DailyCodingProblem.Problems.Problem00011
+{
+    [MemoryDiagnoser]
+    public class Benchmark : IBenchmark
+    {
+        public string printBefore() => "";
+        public string printAfter() => "";
+
+        [Params(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)]
+        public int entryLength;
+
+        ISolution searchTree = new SearchTree();
+
+        string entry;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            entry = new string('B', entryLength);
+            searchTree.Add(entry);
+        }
+
+        [Benchmark]
+        public void SearchTree() => searchTree.Search(entry);
+    }
+}
