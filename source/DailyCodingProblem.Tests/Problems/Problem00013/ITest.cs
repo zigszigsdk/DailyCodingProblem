288f408c08f4a17dedae5ef79451a6d18fa36b2c
Chris Jensen <zigs@zigs.dk>
2018-11-02 09:46:20 +0100

    problem 13 tests

---
+++ source/DailyCodingProblem.Tests/Problems/Problem00013/ITest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DailyCodingProblem.Tests.Problems.Problem00013
+{
+    public interface ITest
+    {
+        void GivenTest();
+        void BestAtStart();
+        void BestAtEnd();
+        void ZeroAllowed();
+        void NullString();
+        void EmptyString();
+        void ShorterString();
+        void NoRepeats();
+    }
+}
---
+++ source/DailyCodingProblem.Tests/Problems/Problem00013/Tests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+using DailyCodingProblem.Problems.Problem00013;
+
+namespace DailyCodingProblem.Tests.Problems.Problem00013
+{
+    public class Tests
+    {
+        public void GivenTest(ISolution solution)
+            => Assert.Equal(3, solution.Solve(2, "abcba"));
+
+        public void BestAtStart(ISolution solution)
+            => Assert.Equal(3, solution.Solve(2, "abacd"));
+
+        public void BestAtEnd(ISolution solution)
+            => Assert.Equal(3, solution.Solve(2, "deaba"));
+
+        public void ZeroAllowed(ISolution solution)
+            => Assert.Equal(0, solution.Solve(0, "abcba"));
+
+        public void NullString(ISolution solution)
+            => Assert.Equal(0, solution.Solve(2, null));
+
+        public void EmptyString(ISolution solution)
+            => Assert.Equal(0, solution.Solve(2, ""));
+
+        public void ShorterString(ISolution solution)
+            => Assert.Equal(3, solution.Solve(5, "abc"));
+
+        public void NoRepeats(ISolution solution)
+            => Assert.Equal(3, solution.Solve(3, "abcdef"));
+
+
+
+
+
+
+
+    }
+}
