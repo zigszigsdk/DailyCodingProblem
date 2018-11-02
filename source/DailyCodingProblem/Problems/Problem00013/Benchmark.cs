33cdf7cfd655284b0186e0d6c89791e3f0409e74
Chris Jensen <zigs@zigs.dk>
2018-11-02 09:47:01 +0100

    problem 13 benchmark

---
+++ source/DailyCodingProblem/Problems/Problem00013/Benchmark.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using DailyCodingProblem.Problems.Problem00013.Solutions;
+
+namespace DailyCodingProblem.Problems.Problem00013
+{
+    [MemoryDiagnoser]
+    public class Benchmark : IBenchmark
+    {
+        public string printBefore() => "";
+        public string printAfter() => "";
+
+        [Params(10, 20, 30, 40, 50)]
+        public int stringLength;
+
+        string input;
+
+        [GlobalSetup]
+        public void GlobalSetup() => input = new string('a', stringLength);
+
+        ISolution bruteforce = new Bruteforce();
+
+        [Benchmark]
+        public int Bruteforce() => bruteforce.Solve(3, input);
+    }
+}
