54959e8cf6e339b50a2446bb09efcaca367c24fe
Chris Jensen <zigs@zigs.dk>
2018-11-02 18:11:39 +0100

    better worst case test for "snake" solution on problem 13

--- source/DailyCodingProblem/Problems/Problem00013/Benchmark.cs
+++ source/DailyCodingProblem/Problems/Problem00013/Benchmark.cs
@@ -1,4 +1,6 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System;
+
+using BenchmarkDotNet.Attributes;
 
 using DailyCodingProblem.Problems.Problem00013.Solutions;
 
@@ -13,18 +15,26 @@ namespace DailyCodingProblem.Problems.Problem00013
         [Params(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)]
         public int stringLength;
 
-        string input;
+        string bruteforceInput;
+        string snakeInput = "";
 
         [GlobalSetup]
-        public void GlobalSetup() => input = new string('a', stringLength);
+        public void GlobalSetup()
+        {
+            bruteforceInput = new string('a', stringLength);
+
+            Random random = new Random();
+            while (snakeInput.Length != stringLength)
+                snakeInput += (char)random.Next('a', 'z');   
+        }
 
         ISolution bruteforce = new Bruteforce();
         ISolution snake = new Snake();
 
         [Benchmark]
-        public int Bruteforce() => bruteforce.Solve(3, input);
+        public int Bruteforce() => bruteforce.Solve(3, bruteforceInput);
 
         [Benchmark]
-        public int Snake() => snake.Solve(3, input);
+        public int Snake() => snake.Solve(3, snakeInput);
     }
 }
