bb3dcd7bb96a7eb77b61b39bc9883c7907dba553
Chris Jensen <zigs@zigs.dk>
2018-11-02 09:46:40 +0100

    problem 13 solution: bruteforce

---
+++ source/DailyCodingProblem/Problems/Problem00013/Solutions/Bruteforce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Problems.Problem00013.Solutions
+{
+    public class Bruteforce : ISolution
+    {
+        public int Solve(int maxChars, string subject)
+        {
+            if (subject == null || subject.Length == 0 || maxChars <= 0)
+                return 0;
+
+            if (maxChars >= subject.Length)
+                return subject.Length;
+
+            int result = maxChars;
+
+            HashSet<char> seen;
+            int next;
+            bool roomForNextChar() => seen.Count != maxChars || seen.Contains(subject[next]);
+
+            for (int start = subject.Length - 1 - maxChars; start >= 0; start--)
+            {
+                seen = new HashSet<char>();
+
+                for (next = start; next < subject.Length && roomForNextChar(); next++)
+                    seen.Add(subject[next]);
+
+                if (next - start > result)
+                    result = next - start;
+            }
+
+            return result;
+        }
+    }
+}
